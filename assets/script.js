b34dd795b36564d874fcc32cfef8b7745b425ad5
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-07-25 17:42:10 -0400

    spelling error

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         event.preventDefault();
         // API Keys and their documentation
         // https://openweathermap.org/api/one-call-api#how
-        const weatherApiKEy = "95f8c20b3dbfd6ef88b8d05477161fb2";
+        const weatherApiKey = "95f8c20b3dbfd6ef88b8d05477161fb2";
         // https://positionstack.com/quickstart
         const geocodeApiKey = "05f7e859be5401b4c8ef273ab790760d";
         // takes the value of the weather input.
@@ -17,7 +17,7 @@ $(document).ready(function() {
             const lat = returnedLatLong.data[0].latitude;
             const long = returnedLatLong.data[0].longitude;
             console.log(lat,long);
-            const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=${weatherApiKEy}`;
+            const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=${weatherApiKey}`;
             // Uses newly acquired lat/long to find weather for the location.
             $.get(weatherURL).then(function(returnedWeather) {
                 console.log(returnedWeather);
