96829c629e52de5aada469eb558335d66e6f892b
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-07-26 18:10:03 -0400

    Trying new geocode service

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     // https://openweathermap.org/api/one-call-api#how
     const weatherApiKey = "95f8c20b3dbfd6ef88b8d05477161fb2";
     // https://positionstack.com/quickstart
-    const geocodeApiKey = "05f7e859be5401b4c8ef273ab790760d";
+    // const geocodeApiKey = "05f7e859be5401b4c8ef273ab790760d";
+    const geocodeApiKey = "09139eed245840c0ae1f7e7e3ed56de8";
     // All of the current weather hooks
     const currentDate = moment().format('L');
     const cityAndDate = $('h2.cityDateValue');
@@ -38,11 +39,13 @@ $(document).ready(function() {
         const locationInput = $('input').val();
         console.log(locationInput);
         // Grabs the Lat/Long of the input location.
-        const queryURL = `https://api.positionstack.com/v1/forward?access_key=${geocodeApiKey}&query=${locationInput}`;
+        // const queryURL = `https://api.positionstack.com/v1/forward?access_key=${geocodeApiKey}&query=${locationInput}`;
+        const queryURL = `https://api.opencagedata.com/geocode/v1/json?q=${locationInput}&key=${geocodeApiKey}`;
         // Querys the geocode API
         $.get(queryURL).then(function(returnedLatLong) {
-            const lat = returnedLatLong.data[0].latitude;
-            const long = returnedLatLong.data[0].longitude;
+            console.log(returnedLatLong)
+            const lat = returnedLatLong.results[0].geometry.lat;
+            const long = returnedLatLong.results[0].geometry.lng;
             console.log(returnedLatLong);
             const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=${weatherApiKey}&units=imperial`;
             // Uses newly acquired lat/long to find weather for the location.
