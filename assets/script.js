f3a176cb578f2e538802ff66aa39b88ccc155a59
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-07-26 17:34:21 -0400

    activated button rendering

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    // API Keys and their documentation
+    // https://openweathermap.org/api/one-call-api#how
+    const weatherApiKey = "95f8c20b3dbfd6ef88b8d05477161fb2";
+    // https://positionstack.com/quickstart
+    const geocodeApiKey = "05f7e859be5401b4c8ef273ab790760d";
     // All of the current weather hooks
     const currentDate = moment().format('L');
     const cityAndDate = $('h2.cityDateValue');
@@ -17,14 +22,18 @@ $(document).ready(function() {
     // Button creation for the search history
     const buttonRow = $('div.buttonRow');
     const historyButton = $('<button>').addClass('btn btn-light btn-lg btn-block historyButton');
-    
+    console.log(searchHistory);
+    function renderButtons() {
+        $(buttonRow).empty();
+        searchHistory.forEach(function(item) {
+            console.log(item);
+            historyButton.attr('data-lat', item.lat).attr('data-long', item.long).attr('value', item.value).text(item.value);
+            buttonRow.prepend(historyButton);
+        })
+    }
+    renderButtons();
     $('button.weatherSubmit').on("click", function(event) {
         event.preventDefault();
-        // API Keys and their documentation
-        // https://openweathermap.org/api/one-call-api#how
-        const weatherApiKey = "95f8c20b3dbfd6ef88b8d05477161fb2";
-        // https://positionstack.com/quickstart
-        const geocodeApiKey = "05f7e859be5401b4c8ef273ab790760d";
         // takes the value of the weather input.
         const locationInput = $('input').val();
         console.log(locationInput);
@@ -62,6 +71,30 @@ $(document).ready(function() {
             })
         })
     })
+    $(document).on("click", "button.historyButton", function(event) {
+        event.preventDefault();
+        const targetButton = event.target;
+        const lat = $(targetButton).attr('data-lat');
+        const long = $(targetButton).attr('data-long');
+        console.log(lat,long);
+        const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=${weatherApiKey}&units=imperial`;
 
-    
+        $.get(weatherURL).then(function(returnedWeather) {
+            console.log(returnedWeather);
+            const { current: {temp, wind_speed, uvi, weather: {[0]:{icon}}}, daily} = returnedWeather;
+            cityAndDate.text(`${targetButton.value} (${currentDate})`);
+            currentIcon.attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
+            currentTemp.text(temp);
+            currentWind.text(wind_speed);
+            currentUV.text(uvi);
+
+            forecastContent.each(function(forecastDay) {
+                console.log(forecastDay);
+                $(forecastDate[forecastDay]).text(moment().add(forecastDay, 'days').format('ddd'));
+                $(forecastIcon[forecastDay]).attr("src", `http://openweathermap.org/img/wn/${daily[forecastDay].weather[0].icon}@2x.png`);
+                $(forecastTemp[forecastDay]).text(daily[forecastDay].temp.day);
+                $(forecastHumidity[forecastDay]).text(daily[forecastDay].humidity);
+            })
+    })
+})   
 })
